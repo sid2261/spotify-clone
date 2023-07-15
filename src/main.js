3c27d47b0a528a1398660d84d2bb56748e3dcb13
sid2261 <siddhartha2251@gmail.com>
2023-07-15 14:43:41 +0530

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.addEventListener("DOMContentLoaded", () =>{
+  if (localStorage.getItem("acessToken")){
+    window.location.href = "dashboard/dashboard.html";
+  }else{
+    window.location.href = "login/login.html";
+  }
+})
+
+
