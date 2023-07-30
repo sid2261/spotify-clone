6f9dee4e879e082af515c576058e7462ac64dcd5
sid2261 <siddhartha2251@gmail.com>
2023-07-30 12:17:24 +0530

    Created playlists

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     extend: {
       colors: {
         "green": "#1DB954",
-        "dark" : "#191414"
+        "black" : "#191414",
+        "light-black" : "#282828",
+        "primary" : "#FFFFFF",
+        "secondary" : "#b3b3b3",
+        "gray" : "#535353"
+      },
+      gridTemplateColumns:{
+        'auto-fill-cards': 'repeat(auto-fill, minmax(200px, 1fr))'
       }
     },
   },
